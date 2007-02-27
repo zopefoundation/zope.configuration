6cd0ed414226b8e8f8dbae4faf2fead78b1b39f8
Benji York <benji@benjiyork.com>
2007-02-27 02:56:25 +0000

    initial buildoutification of zope.configuration

--- setup.py
+++ setup.py
@@ -24,23 +24,13 @@ setup(name='zope.configuration',
       version='3.4dev',
       url='http://svn.zope.org/zope.configuration',
       license='ZPL 2.1',
-      description='Zope Configuration (ZCML)',
-      author='Zope Corporation and Contributors',
+      description=open("README.txt").read(),
+      long_description=open("src/zope/configuration/README.txt").read(),
+      author='Zope Project',
       author_email='zope3-dev@zope.org',
-      long_description='''\
-The zope configuration system provides an extensible system for
-supporting various kinds of configurations.
-
-It is based on the idea of configuration directives. Users of the
-configuration system provide configuration directives in some
-language that express configuration choices. The intent is that the
-language be pluggable.  An XML language is provided by default.
-      ''',
-      
-	  packages=find_packages('src'),
+      packages=find_packages('src'),
       package_dir = {'': 'src'},
-
-      namespace_packages=['zope',],
+      namespace_packages=['zope'],
       tests_require = ['zope.testing'],
       install_requires=['zope.deprecation',
                         'zope.i18nmessageid',
@@ -48,6 +38,6 @@ language be pluggable.  An XML language is provided by default.
                         'zope.schema',
                        ],
       include_package_data = True,
-
       zip_safe = False,
+      keywords="zope zope3 configuration",
       )
