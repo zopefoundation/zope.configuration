abbfa9b90edae4a03025b65e4cf7de0d1e1cbd0b
Stephan Richter <srichter@gmail.com>
2007-10-02 05:04:59 +0000

    Get ready for 3.4.0 release.

--- README.txt
+++ README.txt
@@ -11,5 +11,3 @@ It is based on the idea of configuration directives. Users of the
 configuration system provide configuration directives in some
 language that express configuration choices. The intent is that the
 language be pluggable.  An XML language is provided by default.
-
-See 'src/zope/configuration/README.txt' for more information.
--- setup.py
+++ setup.py
@@ -15,23 +15,45 @@
 
 $Id$
 """
-
 import os
-
 from setuptools import setup, find_packages
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 setup(name='zope.configuration',
-      version = '3.5.0a1',
-      url='http://svn.zope.org/zope.configuration',
-      license='ZPL 2.1',
-      description=open("README.txt").read(),
-      long_description=open("src/zope/configuration/README.txt").read(),
-      author='Zope Project',
+      version = '3.4.0',
+      author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
+      description='Zope Configuration Markup Language (ZCML)',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          'Detailed Documentation\n' +
+          '----------------------\n'
+          + '\n\n' +
+          read('src', 'zope', 'configuration', 'README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 configuration zcml",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.configuration',
+      license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
       namespace_packages=['zope'],
-      #tests_require = ['zope.testing'],
+      extras_require = dict(
+          test=['zope.testing']),
       install_requires=['zope.deprecation',
                         'zope.i18nmessageid',
                         'zope.interface',
@@ -40,5 +62,4 @@ setup(name='zope.configuration',
                        ],
       include_package_data = True,
       zip_safe = False,
-      keywords="zope zope3 configuration",
       )
