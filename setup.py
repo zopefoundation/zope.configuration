3a684d1ecf5490b85fff1a2ddf6079b8e2eaff09
Chris McDonough <chrism@plope.com>
2011-12-06 09:06:29 +0000

    prep for 3.8.0 tag

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def alltests():
     return suite
 
 setup(name='zope.configuration',
-      version = '3.8.0dev',
+      version = '3.8.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Configuration Markup Language (ZCML)',
