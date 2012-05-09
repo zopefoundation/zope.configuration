9f4cb5e311a6bc22e1057327bec73badcba64a29
Tres Seaver <tseaver@palladion.com>
2012-05-09 23:46:41 +0000

    Full coverage for z.c.name.resolve.

--- src/zope/configuration/tests/test_name.py
+++ src/zope/configuration/tests/test_name.py
@@ -22,6 +22,48 @@ class Test_resolve(unittest.TestCase):
         from zope.configuration.name import resolve
         return resolve(*args, **kw)
 
+    def test_top_level_module(self):
+        import os
+        self.assertTrue(self._callFUT('os') is os)
+
+    def test_nested_module(self):
+        import os.path
+        self.assertTrue(self._callFUT('os.path') is os.path)
+
+    def test_function_in_module(self):
+        import os.path
+        self.assertTrue(self._callFUT('os.path.join') is os.path.join)
+
+    def test_importable_but_not_attr_of_parent(self):
+        import sys
+        import zope.configuration.tests as zct
+        self.assertFalse('notyet' in zct.__dict__)
+        mod = self._callFUT('zope.configuration.tests.notyet')
+        self.assertTrue(mod is zct.notyet)
+        del zct.notyet
+        del sys.modules['zope.configuration.tests.notyet']
+
+    def test_function_in_module_relative(self):
+        import os.path
+        self.assertTrue(self._callFUT('.join', 'os.path') is os.path.join)
+
+    def test_class_in_module(self):
+        from zope.configuration.tests.directives import Complex
+        self.assertTrue(
+            self._callFUT('zope.configuration.tests.directives.Complex')
+                    is Complex)
+
+    def test_class_w_same_name_as_module(self):
+        from zope.configuration.tests.samplepackage.NamedForClass \
+            import NamedForClass
+        self.assertTrue(
+            self._callFUT(
+                'zope.configuration.tests.samplepackage.NamedForClass+')
+                    is NamedForClass)
+        self.assertTrue(
+            self._callFUT(
+                'zope.configuration.tests.samplepackage.NamedForClass.')
+                    is NamedForClass)
 
 class Test_getNormalizedName(unittest.TestCase):
 
