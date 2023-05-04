e29f77dbe92d1d316076f3886594da0b6e897c0c
Michael Howitz <mh@gocept.com>
2023-05-04 08:17:35 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.configuration',
-      version='5.0',
+      version='5.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope Configuration Markup Language (ZCML)',
