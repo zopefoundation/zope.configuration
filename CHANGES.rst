7a103b82b7a62ce0ae3cf180e8a730964fa3b23f
Michael Howitz <mh@gocept.com>
2023-05-04 08:17:16 +0200

    Preparing release 5.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changes
 =======
 
-5.0 (unreleased)
+5.0 (2023-05-04)
 ----------------
 
 - Drop support for Python 2.7, 3.5, 3.6.
--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.configuration',
-      version='5.0.dev0',
+      version='5.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope Configuration Markup Language (ZCML)',
